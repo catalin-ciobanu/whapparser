ca740a8aa05bba513b5cc7697f444cc57d9b2b20
Catalin Ciobanu <catalin.ciobanu@hindawi.com>
2023-01-17 14:55:55 +0200

    added comments for starting the node server and connecting to mysql console

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//DEBUG=express-locallibrary-tutorial:* npm run devstart --inspect
+//npm run serverstart
+//mysql -u root -p
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
