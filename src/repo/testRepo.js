4d7bdc2a6267998a306330b5930356cb71ea71f0
Catalin Ciobanu <catalin-ciobanu@users.noreply.github.com>
2024-03-27 09:33:00 +0200

    Update testRepo.js
    sters info comments

--- src/repo/testRepo.js
+++ src/repo/testRepo.js
@@ -3,15 +3,6 @@ const expenseRepo = require('./expenseRepo');
 
 console.log("creating")
 console.log(expenseRepo.createExpense({
-    description: "Something", name: "Catalin",
-    type: "Farmacie", bucket: "Cheltuieli", sum: 12, expense_date: "01.01.2021"
+    description: "Something", name: "SOmeone",
+    type: "Cumparaturi", bucket: "Cheltuieli", sum: 12, expense_date: "01.01.2021"
 }));
-
-// console.log("deleting with id 1")
-// console.log(expenseRepo.deleteExpenseById(2));
-
-// console.log("update with id 3");
-// console.log(expenseRepo.updateExpense(3, {
-//     description: "Something Back", name: "Elena",
-//     type: "Farmacie", bucket: "Cheltuieli", sum: 10, expense_date: "01.01.2021"
-// }));
